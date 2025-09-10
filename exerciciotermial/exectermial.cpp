691c036af1a411af385c1489c23e333af03192a9
vitor gomes <vitor.araujo.derreteman4@gmail.com>
2025-09-10 01:42:11 -0300

    update exectermial.cpp

--- exerciciotermial/exectermial.cpp
+++ exerciciotermial/exectermial.cpp
@@ -2,17 +2,18 @@
 
 unsigned long long int termial(int termial){ //Função para calcular o termial de n
 
-    unsigned long long int total = 0; //valor neutro para soma, unsigned apenas para valores positivos, e long long para numeros absurdos de grande
+    unsigned long long int total = 0; //valor neutro para soma
 
     for (int i = termial; i > 0; i--) {
         total += i;
     }
+    
     return total;
 }
 
 unsigned long long int fatorial(int fatorial){ //Função para calcular o fatorial de n
     
-    unsigned long long total = 1; //valor neutro para multiplicação, unsigned apenas para valores positivos, e long long para numeros absurdos de grande
+    unsigned long long total = 1; //valor neutro para multiplicação
 
     for (int i = fatorial; i > 0; i--) {
         total = total * i;
@@ -37,25 +38,48 @@ int main(){
         scanf("%hu", &choice);
 
         switch (choice) {
+
+
             case 1: //No caso 1 é calculado o n? e depois exibido o resultado
 
-                printf("\n\nDigite um numero inteiro positivo a ser calculado o termial: ");
-                scanf("%d", &parametro_termial);
+                do {
+                    printf("\n\nDigite um numero inteiro positivo a ser calculado o termial: ");
+                    scanf("%d", &parametro_termial);
+                    
+                    if (parametro_termial > 0) { //Se o numero for valido para a operação a gente quebra o loop
+                        break;
+                    }else { //Se não a exibe um mensagem dizendo que o valor é invalido e continua a pedir
+                        printf("\nERRO");
+                        printf("\n\nDigite um valor inteiro positivo maior que 0\n\n");
+                    }
+                }while (true);
 
-                termial_resultado = termial(parametro_termial);
+
+                termial_resultado = termial(parametro_termial); //processando o fatorial do numero e em seguida exibindo
                 printf("\n\n%d? = %lld", parametro_termial, termial_resultado);
 
                 break;
             
+
             case 2://No caso 2 é calculado o n! e depois exibido o resultado
                 
-                printf("\n\nDigite um numero inteiro positivo a ser calculado o fatorial (maximo que consegue exibir é 20): ");
-                scanf("%d", &parametro_fatorial);
+                do {
+                    printf("\n\nDigite um numero inteiro positivo a ser calculado o fatorial (maximo que consegue exibir é 20): ");
+                    scanf("%d", &parametro_fatorial);
 
-                fatorial_resultado = fatorial(parametro_fatorial);
+                    if (parametro_fatorial > 0) { //Se o numero for valido para a operação a gente quebra o loop
+                        break;
+                    }else { //Se não a exibe um mensagem dizendo que o valor é invalido e continua a pedir
+                        printf("\nERRO");
+                        printf("\nDigite um valor inteiro positivo maior que 0\n\n");
+                    }
+                }while (true);
+
+                fatorial_resultado = fatorial(parametro_fatorial); //processando o fatorial do numero e em seguida exibindo
                 printf("\n\n%d? = %lld", parametro_fatorial, fatorial_resultado);
 
                 break;
+
 
             case 3: //Nesse caso a execução é terminada
                 printf("\nTerminando execução..\n");
