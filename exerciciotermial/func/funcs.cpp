328145ef434b2c80dbc96807338027f68b45cf93
vitor gomes <vitor.araujo.derreteman4@gmail.com>
2025-09-23 17:47:27 -0300

    terminando todas as funções recursivas

--- exerciciotermial/func/funcs.cpp
+++ exerciciotermial/func/funcs.cpp
@@ -1,6 +1,6 @@
 #include <cstdlib>
-
-unsigned long long termial(int termial){ //Função para calcular o termial de n
+/*
+unsigned long long int termial(int termial){ //Função para calcular o termial de n
 
     unsigned long long int total = 0; //valor neutro para soma
 
@@ -40,4 +40,56 @@ float exponencial_negativo(int base, int expoente){
 
     total = 1/total;
     return total;
+}
+*/
+
+unsigned long long int termial(int numero){ //Função para calcular o termial de n
+
+    if (numero <= 0) {
+        return 0;
+    }
+    
+    return numero + termial(numero - 1);
+}
+
+unsigned long long fatorial(int numero){ //Função para calcular o fatorial de n
+
+    if (numero <= 1) {
+        return 1;
+    }
+    return numero * fatorial(numero - 1);
+}
+
+long long exponencial(int base, int expoente){ //Função para calcular o exponencial de n
+    
+    if (expoente == 0) {
+        return 1;
+    }
+
+    if (expoente == 1) {
+        return base;
+    }
+
+    return base * exponencial(base, expoente - 1);
+}
+
+float exponencial_negativo(int base, int expoente){ //função para calcular a base elevada a expoentes negativos inteiros
+
+    int abs_expoente = abs(expoente);
+
+    if (abs_expoente == 0) {
+        return 1;
+    }
+
+    if (abs_expoente == 1) {
+        return base;
+    }
+
+    float resultado = base * exponencial(base, abs_expoente - 1);
+
+    if (expoente < 0) {
+        return 1/resultado;
+    }else {
+        return resultado;
+    }
 }
